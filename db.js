a5c0495fd2d5ee8a4dde0670f98282891ada6ccb
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-04-25 20:37:20 +0200

    validation implemented

--- db.js
+++ db.js
@@ -6,24 +6,38 @@ mongoose.connect('mongodb://localhost:27017/mammaterra-catalogue');
 
 const GiftSet = mongoose.model('GiftSet', {
     slug: {
-        type: String
+        type: String,
+        required: [true, 'Slug jest wymagany'],
+        minLength: [3, 'minimalna liczba znaków to 3']
     },
     name: {
-        type: String
+        type: String,
+        required: [true, 'Nazwa jest wymagana']
     },
     price: {
-        type: Number
+        type: Number,
+        required: [true, 'Cena jest wymagana'],
+        min: [1, 'Cena musi być większa niż 0']
     }});
 
 async function main() {
 
     const giftSet = new GiftSet({
-        slug:'small',
-        name:'mały zestaw',
-        price: 50
+        slug:'medium',
+        name:'średni zestaw',
+        price: 150
     })
 
-   await giftSet.save();
+    try {
+        await giftSet.save();
+
+    }catch (err) {
+        console.log('coś poszło nie tak...');
+        for (const key in err.errors) {
+                console.log(err.errors[key].message);
+            
+        }
+    }
 }
 
 main();
