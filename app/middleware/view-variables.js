176a06b97bd2f9ad4f83dcbfb612d65f9ca22e35
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-05-28 21:30:22 +0200

    search logic implemented

--- app/middleware/view-variables.js
+++ app/middleware/view-variables.js
@@ -2,5 +2,6 @@ module.exports = function (req, res, next) {
         res.locals.url = req.url;
         res.locals.errors = null;
         res.locals.form = {};
+        res.locals.query = req.query;
         next();
       }
