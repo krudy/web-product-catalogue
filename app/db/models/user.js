51f4eaf5b2fa666d07d70877e1b0b2dc6204d396
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-07-23 16:18:59 +0200

    hashing of the password before saving in the database has been implemented

--- app/db/models/user.js
+++ app/db/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 const {validateEmail} = require('../validators');
 
 const userSchema = new Schema({
@@ -16,6 +17,12 @@ const userSchema = new Schema({
         minLenght: [5, "Hasło powinno zawierać przynajmniej 4 znaki"]
     }
 });
+
+userSchema.path('password').set((value) => {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(value, salt);
+    return hash
+})
 
 userSchema.post('save', function(error, doc, next) {
     if(error.code === 11000) {
